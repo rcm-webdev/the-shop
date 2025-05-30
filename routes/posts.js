d59c53ecb016e5211d317afc16ed4b3bfcd441e3
Roberto Murillo <rcm.webdev.pro@gmail.com>
2025-05-30 00:14:52 -0700

    modify post routes to handle multiple file uploads using upload.fields()

--- routes/posts.js
+++ routes/posts.js
@@ -7,10 +7,13 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Post Routes - simplified for now
 router.get("/:id", ensureAuth, postsController.getPost);
 
-router.post("/createPost", upload.single("file"), postsController.createPost);
+router.post("/createPost", upload.fields([
+  { name: 'frontImage', maxCount: 1 },
+  { name: 'backImage', maxCount: 1 }
+]), postsController.createPost);
 
-router.put("/likePost/:id", postsController.likePost);
+router.put("/likePost/:id", ensureAuth, postsController.likePost);
 
-router.delete("/deletePost/:id", postsController.deletePost);
+router.delete("/deletePost/:id", ensureAuth, postsController.deletePost);
 
 module.exports = router;

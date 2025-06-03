eee04a09e099f8a40ff89602311e26c4c1fc816c
aokiji <rcm.webdev.pro@gmail.com>
2025-06-02 20:03:04 -0700

    update post schema with other fields that will match with google-sheet-api

--- models/Post.js
+++ models/Post.js
@@ -29,6 +29,34 @@ const PostSchema = new mongoose.Schema({
     type: [String],
     default: [],
   },
+  wheelVariations: {
+    type: [String],
+    default: [],
+  },
+  toyNumber: {
+    type: String,
+    required: false,
+  },
+  year: {
+    type: Number,
+    required: false,
+  },
+  series: {
+    type: String,
+    required: false,
+  },
+  condition: {
+    type: String,
+    required: false,
+  },
+  isSold: {
+    type: Boolean,
+    default: false,
+  },
+  boxNumber: {
+    type: String,
+    required: false,
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
